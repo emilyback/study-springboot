14ab65cf571b5c6ecb75e3c6ec515c375db950d0
rhdiddl501 <rhdiddl501@naver.com>
2020-04-20 11:22:17 +0900

    login result method

--- back-end/src/main/java/com/cpst/demo/DAO/MemberDAO.java
+++ back-end/src/main/java/com/cpst/demo/DAO/MemberDAO.java
@@ -1,28 +1,53 @@
 package com.cpst.demo.DAO;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 
-import com.cpst.demo.DTO.*;
+import org.springframework.stereotype.Repository;
+
+import com.cpst.demo.DTO.MemberDTO;
+
+@Repository
 public class MemberDAO {
 	
-	MemberDTO member;
-	HashMap<String,MemberDTO> memberList;
 	
-	public MemberDAO(HashMap<String,MemberDTO> memberList) {
-		memberList = getMembers();
+	HashMap<String,MemberDTO> memberList = new HashMap<String, MemberDTO>();
+	
+	public MemberDAO() {
+		setMembers();
 	}
 	
 	public HashMap<String,MemberDTO> getMembers() {
-		member = new MemberDTO("박보검", "bbg", "bbg123");
-		memberList.put("0",member);
 		
-		member = new MemberDTO("여진구","yjg","yjg123");
-		memberList.put("1",member);
-		
-		member = new MemberDTO("주지훈","jjh","jjh123");
-		memberList.put("2",member);
-
 		return memberList;
 	}
+	
+	public void setMembers() {
+		MemberDTO member;
+		member = new MemberDTO("박보검", "bbg", "bbg123");
+		memberList.put(member.getId(),member);
+		
+		member = new MemberDTO("여진구","yjg","yjg123");
+		memberList.put(member.getId(),member);
+		
+		member = new MemberDTO("주지훈","jjh","jjh123");
+		memberList.put(member.getId(),member);
+	}
+	
+	public MemberDTO memberCheck(HashMap<String, String> loginParam) {
+		MemberDTO memberDTO = null;
+		
+		String loginId = loginParam.get("loginId");
+		String loginPwd = loginParam.get("loginPwd");
+		
+		if(memberList.containsKey(loginId)) {
+			if(memberList.get(loginId).getPwd().equals(loginPwd))
+				memberDTO = memberList.get(loginId);
+		}
+
+		return memberDTO;
+	}
+	
 	
 }
